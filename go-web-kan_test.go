4e664f6a8f9b22f5a6023e5a0f30fbcd1105ebe6
Kan Nakamura <knakamura@pivotal.io>
2016-12-11 22:09:50 +0900

    12/11 10:09pm added test.go

---
+++ go-web-kan_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "net/http"
+    "net/http/httptest"
+    "testing"
+)
+
+func Test_handler(t *testing.T) {
+    req, err := http.NewRequest("GET", "/", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    res := httptest.NewRecorder()
+    handler(res, req)
+
+    exp := "Hello, World v8"
+    act := res.Body.String()
+    if exp != act {
+        t.Fatalf("Expected %s gog %s", exp, act)
+    }
+}
