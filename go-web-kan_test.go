5084672a2ddc72b250c4236be5a9e7963eb45213
Kan Nakamura <knakamura@pivotal.io>
2016-12-12 15:30:55 +0900

    v11 to v12

--- go-web-kan_test.go
+++ go-web-kan_test.go
@@ -15,7 +15,7 @@ func Test_handler(t *testing.T) {
     res := httptest.NewRecorder()
     handler(res, req)
 
-    exp := "Hello, World v11"
+    exp := "Hello, World v12"
     act := res.Body.String()
     if exp != act {
         t.Fatalf("Expected %s gog %s", exp, act)
