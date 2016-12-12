d89350ed1466790a3488c2eb68e953cf7edfbc78
Kan Nakamura <knakamura@pivotal.io>
2016-12-12 15:21:55 +0900

    v9 to v10

--- go-web-kan.go
+++ go-web-kan.go
@@ -8,7 +8,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World v9")
+	fmt.Fprintf(w, "Hello, World v10")
 }
 
 func main() {
---
+++ task/update-route.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -xe
+
+pwd
+env
+
+cf api https://api.run.pivotal.io --skip-ssl-validation
+
+cf login -u knakamura@pivotal.io -p passw0rd -o APJ -s Development
+
+cf apps |  grep -i go-web-kan
+
+cf routes | grep -i go-web-kan
+
+cf map-route go-web-app-new cfapps.io --hostname go-web-kan
+
+cf routes | grep -i go-web-kan
+
+cf unmap-route go-web-kan cfapps.io --hostname go-web-kan
+
+cf unmap-route go-web-kan-new cfapps.io --hostname go-web-kan-new
