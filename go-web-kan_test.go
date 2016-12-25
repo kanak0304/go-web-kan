c4097bcaccfdc52a16ff1796de76cd0e3d769b1e
Kan Nakamura <knakamura@pivotal.io>
2016-12-25 14:27:46 +0900

    from v6 to v7

--- go-web-kan_test.go
+++ go-web-kan_test.go
@@ -15,7 +15,7 @@ func Test_handler(t *testing.T) {
 	res := httptest.NewRecorder()
 	handler(res, req)
 
-	exp := "Hello, World v6"
+	exp := "Hello, World v7"
 	act := res.Body.String()
 	if exp != act {
 		t.Fatalf("Expected %s gog %s", exp, act)
