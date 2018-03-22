21b4c05956000a22a6ba1ced6715589c003b8937
Kan Nakamura <nakamk2@KannoMacBook-puro.local>
2018-03-22 17:39:03 +0900

    updated cf target to pcf on gcp for update_route.sh

--- task/update-route.sh
+++ task/update-route.sh
@@ -5,21 +5,22 @@ set -xe
 pwd
 env
 
-cf api https://api.run.pivotal.io --skip-ssl-validation
-
-cf login -u knakamura@pivotal.io -p passw0rd -o APJ -s Development
+cf api https://api.cfapps.kanpcf.com --skip-ssl-validation
+cf login -u kan -p password -o biz_div1 -s dev
+#cf api https://api.run.pivotal.io --skip-ssl-validation
+#cf login -u knakamura@pivotal.io -p passw0rd -o APJ -s Development
 
 cf apps |  grep -i go-web-kan
 
 cf routes | grep -i go-web-kan
 
-cf map-route go-web-kan-new cfapps.io --hostname go-web-kan
+cf map-route go-web-kan-new cfapps.kanpcf.com --hostname go-web-kan
 
 cf routes | grep -i go-web-kan
 
-cf unmap-route go-web-kan cfapps.io --hostname go-web-kan
+cf unmap-route go-web-kan cfapps.kanpcf.com --hostname go-web-kan
 
-cf unmap-route go-web-kan-new cfapps.io --hostname go-web-kan-new
+cf unmap-route go-web-kan-new cfapps.kanpcf.com --hostname go-web-kan-new
 
 cf delete go-web-kan -f
 
